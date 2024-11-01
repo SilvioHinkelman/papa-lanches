d245f14bec3b94b1d6a8e27df98ecda4d2344822
SilvioHinkelman <73371769+SilvioHinkelman@users.noreply.github.com>
2024-11-01 14:57:26 -0300

    HistoryType

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,27 @@
+import { IHistory, MockHistory } from "@/DataMock/MockData";
 import Image from "next/image";
-import { MockHistory } from "../DataMock/MockData";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-black">
       <div className=" flex flex-row justify-center p-4 md:x-12">
-        <div className="bg-[#7bbc46] p-4 rounded-md max-w-[1420px] w-full flex justify-between items-center">
-          <Image
-            src="/assets/logo-papa.png"
-            alt="Logo Lanches"
-            width={184}
-            height={184}
-            priority
-          />
-          <div className="flex justify-center items-center bg-orange-200 rounded-full slide-in size-12 md:size-24">
+        <div className="bg-[#7bbc46] p-4 rounded-md max-w-[1420px] w-full flex justify-end">
+          <div className="bg-orange-200 rounded-full slide-in">
+            <Image
+              src="/assets/logo-papa.png"
+              alt="Papa Lanches"
+              width={80}
+              height={8}
+              priority
+              className="max-w-full w-12 md:w-24 "
+            />
             <Image
               src="/assets/papa-lanches.png"
               alt="Papa Lanches"
               width={80}
               height={8}
               priority
+              className="max-w-full w-12 md:w-24 "
             />
           </div>
         </div>
@@ -34,10 +36,10 @@ export default function Home() {
             className="float-left w-40 h-40"
           /> */}
           <div>
-            {MockHistory.map((x) => (
+            {MockHistory.map((x: IHistory) => (
               <h1
                 className="text text-[#ec534d] font-bold indent-8 pb-2 text-justify"
-                key={x.index}
+                key={x.id}
               >
                 {x.text}
               </h1>
